7b1165a4574fa5a778ef0c1e1516adb4da434d55
S0n1c <itss0n1cdev@gmail.com>
2022-01-01 13:42:11 -0500

    More build in apps + manifest

--- api/src/routes/index.ts
+++ api/src/routes/index.ts
@@ -1,6 +1,7 @@
 import { SearchQuery } from 'ipakit';
 import { Request, Router as app } from 'express';
 import { IPAKitAPI } from '..';
+import { manifest } from 'manifest';
 
 export interface ReqInst extends Request {
 	inst: IPAKitAPI
@@ -9,10 +10,31 @@ export interface ReqInst extends Request {
 
 const router = app();
 
+router.get('/icon/:bundle_id', async (req: ReqInst, res) => {
+	const { bundle_id } = req.params;
+	const results = await req.inst.db.search({
+		bundle_id });
+	if (!results.length) {
+		return res.sendStatus(404);
+	}
+	const data = results[0];
+	if (data.icon instanceof Uint8Array) {
+		return res.set('Content-Type', 'image/png').send(Buffer.from(data.icon.buffer));
+	}
+	return res.sendStatus(404);
+});
+
 router.get('/search', async (req: ReqInst, res) => {
 	const { name, bundle_id } = req.query;
-	const results = await req.inst.db.search({ name,
+	const data = await req.inst.db.search({ name,
 		bundle_id });
+	const results = data.map(r => {
+		if (r.icon instanceof Uint8Array) {
+			return { ...r,
+				icon: `${manifest.config.api.url}/icon/${r.bundle_id}` };
+		}
+		return r;
+	});
 	return res.json({ resultCount: results.length,
 		results });
 });
--- package/src/sources/BuiltIn.ts
+++ package/src/sources/BuiltIn.ts
@@ -1,6 +1,18 @@
+import { readdirSync, readFileSync } from 'fs';
 import { readFile } from 'fs/promises';
 import { join } from 'path';
 import { Provider } from '../Provider';
+
+class BuiltInAssets {
+	static get folder(): string {
+		return join(__filename, '../../../assets');
+	}
+
+	static get files(): Record<string, Uint8Array> {
+		return Object.fromEntries(readdirSync(BuiltInAssets.folder, { encoding: 'utf-8',
+			withFileTypes: true }).filter(f => f.isFile() && f.name.endsWith('.png')).flatMap(f => f.name).map(f => [ f.replace('.png', ''), new Uint8Array(readFileSync(join(BuiltInAssets.folder, f)).buffer) ]));
+	}
+}
 
 const BuiltIn = new Provider({
 	name: 'Built-In iOS Apps'
@@ -27,6 +39,7 @@ BuiltIn.run(async (q) => {
 	return find.map(a => ({
 		name: a.name,
 		bundle_id: a.bundle_id,
+		icon: BuiltInAssets.files[a.bundle_id],
 		author: 'Apple'
 	}));
 });
